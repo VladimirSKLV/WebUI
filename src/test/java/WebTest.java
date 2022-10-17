55d9eef060012c0a6936cf32c56bc8844653dba5
VladimirSKLV <vladimirsokolov969@gmail.com>
2022-10-17 10:10:17 +0300

    test

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -15,18 +15,16 @@ public class WebTest {
     @BeforeAll
     static void setUpClass() {
         WebDriverManager.chromedriver().setup();
-        //System.setProperty("webdriver.chrome.driver", "Chromedriver/chromedriver.exe");
     }
 
     @BeforeEach
     void setUpTest() {
-        /*ChromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         options.addArguments("--remote-debugging-port=9999");
-        driver = new ChromeDriver(options);*/
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
 
@@ -37,77 +35,58 @@ public class WebTest {
     }
 
     @Test
-    public void sendFormPositive() throws InterruptedException {
+    public void sendFormPositive() {
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Мак-Кинли Смит");
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79999999990");
-        Thread.sleep(2000); //ставлю остановки для своего удобства
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
-        Thread.sleep(2000);
         driver.findElement(By.cssSelector("[type=\"button\"]")).click();
         String message = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", message.trim());
-        Thread.sleep(2000);
     }
 
     @Test
-    public void sendInvalidName() throws InterruptedException {
+    public void sendInvalidName() {
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Vladimir");
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79999999990");
-        Thread.sleep(2000);
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
-        Thread.sleep(2000);
         driver.findElement(By.cssSelector("[type=\"button\"]")).click();
         String wrong = driver.findElement(By.className("input__sub")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", wrong.trim());
-        Thread.sleep(2000);
     }
 
     @Test
-    public void sendWithoutTel() throws InterruptedException {
-        //List<WebElement> list = driver.findElements(By.className("input__sub"));
+    public void sendWithoutTel() {
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Владимир");
-        Thread.sleep(2000);
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
-        Thread.sleep(1000);
         driver.findElement(By.cssSelector("[type=\"button\"]")).click();
         String wrongTel = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
         assertEquals("Поле обязательно для заполнения", wrongTel.trim());
-        Thread.sleep(2000);
     }
 
     @Test
-    public void sendWithoutName() throws InterruptedException {
-        //List<WebElement> list = driver.findElements(By.className("input__sub"));
+    public void sendWithoutName() {
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79999999990");
-        Thread.sleep(2000);
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
-        Thread.sleep(1000);
         driver.findElement(By.cssSelector("[type=\"button\"]")).click();
         String wrongName = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
         assertEquals("Поле обязательно для заполнения", wrongName.trim());
-        Thread.sleep(2000);
     }
 
     @Test
-    public void sendInvalidTel() throws InterruptedException {
+    public void sendInvalidTel() {
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Мак-Кинли Смит");
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+799999");
-        Thread.sleep(2000); //ставлю остановки для своего удобства
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
-        Thread.sleep(2000);
         driver.findElement(By.cssSelector("[type=\"button\"]")).click();
         String message = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", message.trim());
-        Thread.sleep(2000);
     }
 
     @Test
-    public void withoutAgree() throws InterruptedException {
+    public void withoutAgree() {
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Мак-Кинли Смит");
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79999999990");
-        Thread.sleep(2000); //ставлю остановки для своего удобства
         driver.findElement(By.cssSelector("[type=\"button\"]")).click();
         assertTrue(driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).isDisplayed());
-        Thread.sleep(2000);
     }
 }
